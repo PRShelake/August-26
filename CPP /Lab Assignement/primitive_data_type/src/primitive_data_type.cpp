8cf61f8abfaef848196048e4863a5d7a466eaff9
PRAVEENKUMAR SELKE <119735862+PRShelake@users.noreply.github.com>
2026-08-28 18:02:30 +0530

    Add files via upload

---
+++ CPP /Lab Assignement/primitive_data_type/src/primitive_data_type.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string name = "PRAVEENKUMAR" ;// strng store multiple characters inside double coat
+	char gender = 'm'; // char store only one character inside single coat
+	int age = 25 ;
+	float hieght = 5.8 ;  //double varaible data type is used to more precision
+	double salary = 32000.500 ; //double varaible data type is used to more precision
+	bool isAbuot = true ; // boolean is to true or false value
+
+	cout << "NAME :" << name << endl ;
+	cout << "GENDER :" << gender << endl ;
+	cout << "AGE :" << age << endl ;
+	cout << "HEIGHT :" << hieght << endl ;
+	cout << "SALARY :" << salary << endl ;
+	cout << "Is About:" << isAbuot << endl ;
+	return 0 ;
+
+}
+
