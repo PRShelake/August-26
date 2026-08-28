f9e10bbeef5cc10315d4a862c55ed0f2e4dd5fd9
PRAVEENKUMAR SELKE <119735862+PRShelake@users.noreply.github.com>
2026-08-28 17:55:43 +0530

    Add files via upload

---
+++ CPP /Lab Assignement/Area_of_Rectangle/src/Area_of_Rectangle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int l, b, area;
+
+	cout <<"enter the value of lenght:" << endl;
+	cin>>l;
+
+	cout <<"enter the value of bredth:" << endl;
+	cin>>b;
+
+	area = l*b;
+	cout << "area = " << area << endl;
+
+	return 0;
+}
