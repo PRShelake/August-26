dae86637386f55887af0518a6397046bdd4fc91c
PRAVEENKUMAR SELKE <119735862+PRShelake@users.noreply.github.com>
2026-08-28 18:00:54 +0530

    Add files via upload

---
+++ CPP /Lab Assignement/operator/primitivedatatyoe.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+char name = "PRAVEENKUMAR" ;
+int age = 25 ;
+float hieght = 5.8 ;
+double salary = 32L ;
+bool isAbuot = true ;
+
+cout << "NAME :" << name << endl ;
+cout << "AGE :" << age << endl ;
+cout << "HEIGHT :" << hieght << endl ;
+cout << "SALARY :" << salary << endl ;
+cout << "Is About:" << isAbout << endl ;
+return 0 ;
+
+
+}
---
+++ CPP /Lab Assignement/operator/src/operator.cpp
@@ -0,0 +1,35 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	// declaration variable//
+//	int a = 1; //or we can use this type also int a = 1, b = 3//
+//	int b = 3;
+//
+//	cout << a + b << endl ;//addition operator
+//	cout << a - b << endl ;//addition operator
+//	cout << a * b << endl ;//addition operator
+//	cout << a / b << endl ;//addition operator
+//	cout << a % b << endl ;//addition operator
+//	return 0;
+//}
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// declaration variable//
+	int a = 5; //or we can use this type also int a = 1, b = 3//
+	int b = 2;
+
+	cout << a + b << endl ;//addition operator
+	cout << a - b << endl ;//addition operator
+	cout << a * b << endl ;//addition operator
+	cout << a / b << endl ;//addition operator
+	cout << a % b << endl ;//addition operator
+	cout << -a << endl ;//addition operator
+	return 0;
+}
+
