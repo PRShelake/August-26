e54a2df14a732b61c6b9b0c4d84e00d5bf11b16b
PRAVEENKUMAR SELKE <119735862+PRShelake@users.noreply.github.com>
2026-08-28 17:58:13 +0530

    Add files via upload

---
+++ CPP /Lab Assignement/conditional_statement/src/conditional_statement.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int age = 20;
+
+	//single statement
+	if(age >= 18)
+		cout << age ; // print these vale --> 20
+
+	//multiline if with {}
+	if age(>= )
+
+
+
+
+	return 0;
+}
