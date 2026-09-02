c93f3478a9191d8aee4e472feeca88cb1db490a7
PRAVEENKUMAR SELKE <119735862+PRShelake@users.noreply.github.com>
2026-09-02 11:32:04 +0530

    Add files via upload

---
+++ CPP /Notes C++/Functions in C++/FunctionType4/src/FunctionType4.cpp
@@ -0,0 +1,23 @@
+//============================================================================
+// Name        : FunctionType4.cpp
+// Author      : praveenkumar
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Function without returntype with parameters;
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+//Function without returntype with parameters;
+void add(int a, int b){
+	cout << "sum : " << a+b ;
+}
+
+int main() {
+
+	add(10, 20);
+
+
+	return 0;
+}
