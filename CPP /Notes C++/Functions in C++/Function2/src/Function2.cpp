c6cb79ad91d8886fb21d1e10c1846c58d638445e
PRAVEENKUMAR SELKE <119735862+PRShelake@users.noreply.github.com>
2026-09-02 11:04:58 +0530

    Add files via upload

---
+++ CPP /Notes C++/Functions in C++/Function2/src/Function2.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : Function2.cpp
+// Author      : praveenkumar
+// Version     :
+// Copyright   : Your copyright notice
+// Description : function without return type and without parameters
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+//declaration of function without return type and without parameters
+void add(){
+	cout << "Hello Praveen";
+}
+
+int main() {
+	add();
+	return 0;
+}
